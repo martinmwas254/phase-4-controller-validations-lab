d18f5c5e27e6a6d7036b1581619d341bfbea7a6b
Martin <Martin@Martin>
2023-06-29 04:32:55 +0300

    Completed assignment

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,7 @@
 class AuthorsController < ApplicationController
+
+  rescue_from ActiveRecord::RecordInvalid, with:  :render_unprocessable_entity
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
   
   def show
     author = Author.find(params[:id])
@@ -7,12 +10,21 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    author = Author.create(author_params)
-
+    author = Author.create!(author_params)
     render json: author, status: :created
   end
 
+
   private
+
+  def render_unprocessable_entity(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+  end
+  
+
+  def render_not_found_response
+    render json: { error: "Author not found" }, status: :not_found
+  end
   
   def author_params
     params.permit(:email, :name)
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,42 @@
 class PostsController < ApplicationController
 
-  def show
-    post = Post.find(params[:id])
+  rescue_from ActiveRecord::RecordInvalid, with:  :render_unprocessable_entity
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response 
+  
+  
+    def show
+      post = Post.find(params[:id])
+      
+      render json: post
+    end
+  
+    def update
+      post = Post.find(params[:id])
+  
+      post.update!(post_params)
+  
+      render json: post
+    end
+  
+  
+    def create
+      post = Post.create!(post_params)
+  
+      render json: post, status: :created
+    end
+  
+    private
+  
+    def render_unprocessable_entity(invalid)
+      render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+    end
     
-    render json: post
+    
+  
+ 
+  
+    def post_params
+      params.permit(:category, :content, :title)
+    end
+  
   end
-
-  def update
-    post = Post.find(params[:id])
-
-    post.update(post_params)
-
-    render json: post
-  end
-
-  private
-
-  def post_params
-    params.permit(:category, :content, :title)
-  end
-
-end
